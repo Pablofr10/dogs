257998dadc7991d01706a5e238a8c658c3b89c78
Pablorf10 <rafanet.live@gmail.com>
2022-02-14 21:54:25 -0300

    Criando cadastrar usuário

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../context/UserContext";
+import useForm from "../../hooks/useForm";
+import Button from "../forms/Button";
+import Input from "../forms/Input";
 
 const LoginCreate = () => {
-  function handleSubmit(event) {
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm();
+
+  const { userLogin } = useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+    console.log(response);
   }
 
   return (
     <section className="animeLeft">
-      <h1>Cadastre-se</h1>
-      <form onSubmit={handleSubmit}></form>
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
     </section>
   );
 };
