873fe97faf600940d297de518beebf5e8df03877
Pablorf10 <rafanet.live@gmail.com>
2022-02-14 21:30:50 -0300

    Criando e estilizando login

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const LoginCreate = () => {
-  return <div>Criar Login</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}></form>
+    </section>
+  );
 };
 
 export default LoginCreate;
